54a4557552f908c88aeb1b70f961aba7e2a2a420
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-18 14:25:19 -0600

    Added feature get users

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,8 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
 import { UserType } from './graphql/user.type';
 import { UserService } from './user.service';
 
 @Resolver(() => UserType)
 export class UserResolver {
   constructor(private UserService: UserService) {}
+  @Query(() => [UserType])
+  async getUsers() {
+    const users = await this.UserService.getUsers();
+    return users;
+  }
 }
