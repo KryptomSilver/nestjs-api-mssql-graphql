4eb4d3640bad1c351d8b7b345f41828e4187a76e
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-17 19:34:12 -0600

    Added UserDTO

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,6 @@
+export class UserDTO {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+}
--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
