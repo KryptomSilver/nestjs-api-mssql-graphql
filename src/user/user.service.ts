0f5f5072973c37be25c5d76ee3464e5a3f035a6f
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-17 18:40:20 -0600

    Import user repository in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { IUser } from './interface/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<IUser>,
+  ) {}
+}
