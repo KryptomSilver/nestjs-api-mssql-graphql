6e0aab3b9067d7ccb73f7e30c44c88368a74578f
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-18 14:27:20 -0600

    Added mutation for create user.

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
+import { UserInput } from './graphql/user.input';
 import { UserType } from './graphql/user.type';
 import { UserService } from './user.service';
 
@@ -13,6 +14,12 @@ export class UserResolver {
   @Query(() => UserType, { nullable: true })
   async getUser(@Args('idUser', { type: () => Int }) idUser: number) {
     const user = this.UserService.getUser(idUser);
+    return user;
+  }
+  @Mutation(() => UserType)
+  async createUser(@Args('user') userDTO: UserInput) {
+    const idUser = await this.UserService.createUser(userDTO);
+    const user = await this.UserService.getUser(idUser);
     return user;
   }
 }
