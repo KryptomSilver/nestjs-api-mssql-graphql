9686d5505bd365bda47a05089b57a5eceee8aee8
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-17 18:33:34 -0600

    Configure DB with TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      type: 'mssql',
+      host: `${process.env.DATABASE_HOST}`,
+      port: 1433,
+      username: `${process.env.DATABASE_USER}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database:`${process.env.DATABASE_NAME}`,
+      options: {
+        encrypt: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
