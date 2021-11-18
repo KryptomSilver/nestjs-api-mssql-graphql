448380eb3f5dc0be7a67448940c0b2c6d5ebd8cc
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-17 18:38:46 -0600

    Import user service in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private UserService: UserService) {}
+}
